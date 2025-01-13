292a0eb00b01a9e0311cd3064d01f39134e49dec
Rehan Saleem <rehan.saleem@devdksa.com>
2025-01-14 03:40:18 +0500

    mysql port string to number

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,7 +12,7 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host:  process.env.DATABASE_HOST,
-      port: 3306,
+      port: Number(process.env.DATABASE_PORT),
       username:  process.env.DATABASE_USER,
       password:  process.env.DATABASE_PASSWORD,
       database:  process.env.DATABASE_NAME,
